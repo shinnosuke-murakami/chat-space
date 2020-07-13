4fc9435a04e3bb5eb00510f8b0ce459fb932413f
shinnosuke-murakami <iidabasuke@gmail.com>
2020-07-13 22:01:28 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,23 +26,22 @@ set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
 # デプロイ処理が終わった後、Unicornを再起動するための記述
-after 'deploy:publishing', 'deploy:restart'
-namespace :deploy do
-  task :restart do
-    invoke 'unicorn:restart'
-  end
-end
-
-# secrets.yml用のシンボリックリンクを追加
-set :linked_files, %w{ config/secrets.yml }
-
-# 元々記述されていた after 「'deploy:publishing', 'deploy:restart'」以下を削除して、次のように書き換え
-
 # after 'deploy:publishing', 'deploy:restart'
 # namespace :deploy do
 #   task :restart do
 #     invoke 'unicorn:restart'
 #   end
+# end
+
+# secrets.yml用のシンボリックリンクを追加
+
+# 元々記述されていた after 「'deploy:publishing', 'deploy:restart'」以下を削除して、次のように書き換え
+
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
 
   desc 'upload secrets.yml'
   task :upload do
@@ -56,3 +55,5 @@ set :linked_files, %w{ config/secrets.yml }
   before :starting, 'deploy:upload'
   after :finishing, 'deploy:cleanup'
 end
+
+set :linked_files, %w{ config/secrets.yml }
